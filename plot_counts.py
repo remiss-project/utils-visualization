4b5daee82598526f7b322359d52730ac6e5752eb
Joan Massachs <joan.massachs@eurecat.org>
2023-03-20 16:21:17 +0100

    fa que plot_counts.py sigui compatible amb get_counts_json.py

--- plot_counts.py
+++ plot_counts.py
@@ -12,10 +12,19 @@ def main(infile, outfile):
     counts = pd.read_csv(infile)
     counts['start'] = counts['start'].apply(lambda x: x[:10])
     counts = counts.sort_values(['start', 'query'], ascending=False)
-    ax = sns.lineplot(data=counts, x='start', y='day_count', hue='query')
-    ax.xaxis.set_major_locator(dates.DayLocator(interval=7))
+    granularity = counts.columns[-1].split('_')[0]
+    if 'query' in counts.columns:
+        ax = sns.lineplot(
+            data=counts, x='start', y=granularity+'_counts', hue='query'
+        )
+    else:
+        ax = sns.lineplot(data=counts, x='start', y=granularity+'_counts')
+    if granularity == 'day':
+        ax.xaxis.set_major_locator(dates.DayLocator(interval=7))
+    else:
+        ax.xaxis.set_major_locator(dates.DayLocator(interval=1))
     plt.xticks(rotation=90)
-    plt.xlabel('day')
+    plt.xlabel(granularity)
     plt.ylabel('tweets')
     plt.tight_layout()
     ax.get_figure().savefig(outfile)
