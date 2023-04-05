1ed0d6824e4eb18ee81d82fe4c5f118f780eae2e
Joan Massachs <joan.massachs@eurecat.org>
2023-04-05 13:18:21 +0200

    plot counts: group

--- plot_counts.py
+++ plot_counts.py
@@ -9,15 +9,17 @@ from matplotlib import dates
 @click.argument('infile', type=click.File('r'))
 @click.argument('outfile', type=click.Path())
 @click.option('--xlocator', default=7, show_default=True)
-def main(infile, outfile, xlocator):
+@click.option('--group', is_flag=True)
+def main(infile, outfile, xlocator, group):
     counts = pd.read_csv(infile)
     granularity = counts.columns[-1].split('_')[0]
     y = granularity + '_count'
     counts['start'] = counts['start'].apply(lambda x: x[:10])
     counts = counts.sort_values(['start', y], ascending=[True, False])
-    if 'query' in counts.columns:
+    if 'query' in counts.columns and not group:
         ax = sns.lineplot(data=counts, x='start', y=y, hue='query')
     else:
+        counts = counts[['start', y]].groupby('start').sum()
         ax = sns.lineplot(data=counts, x='start', y=y)
     ax.xaxis.set_major_locator(dates.DayLocator(interval=xlocator))
     plt.xticks(rotation=90)
