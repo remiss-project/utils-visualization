99e7990bc759ae39566f27c05725583db5032ab0
Joan Massachs <joan.massachs@eurecat.org>
2023-04-05 13:16:38 +0200

    plot counts: debug outfile

--- plot_counts.py
+++ plot_counts.py
@@ -7,7 +7,7 @@ from matplotlib import dates
 
 @click.command()
 @click.argument('infile', type=click.File('r'))
-@click.argument('outfile', type=click.File('bw'))
+@click.argument('outfile', type=click.Path())
 @click.option('--xlocator', default=7, show_default=True)
 def main(infile, outfile, xlocator):
     counts = pd.read_csv(infile)
