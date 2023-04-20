e349f7b3d33a6bd226ba966baf63e7c02316bd22
Joan Massachs <joan.massachs@eurecat.org>
2023-04-20 08:57:59 +0200

    plot network

---
+++ plot_network.py
@@ -0,0 +1,76 @@
+import json
+import os
+
+import click
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+def int_to_color(x, colors, level):
+    if type(x) is not int:
+        x = x[level]
+    if colors is not None:
+        with open(colors) as f:
+            low = json.load(f)
+    else:
+        low = {
+            '0': 'tab:blue',
+            '1': 'tab:orange',
+            '2': 'tab:green',
+            '3': 'tab:red',
+            '4': 'tab:purple',
+            '5': 'tab:brown',
+            '6': 'tab:pink',
+            '7': 'tab:olive',
+            '8': 'tab:cyan'
+        }
+    if str(x) in low:
+        return low[str(x)]
+    else:
+        return 'tab:gray'
+
+
+def plot_communities_layout(layout, communities, colors, level, outfile):
+    with open(layout) as f:
+        layout = json.load(f)
+    with open(communities) as f:
+        comm = json.load(f)
+    nodes = sorted(list(layout))
+    x = [layout[n][0] for n in nodes if n in comm]
+    y = [layout[n][1] for n in nodes if n in comm]
+    c = [int_to_color(comm[n], colors, level) for n in nodes if n in comm]
+
+    plt.clf()
+    fig = plt.figure()
+    fig.set_size_inches(20, 20)
+    plt.scatter(x, y, c=c, marker='.', s=1)
+    plt.axis('off')
+    plt.tight_layout()
+    fig.savefig(outfile + '.pdf')
+    fig.savefig(outfile + '.png')
+
+
+@click.command()
+@click.argument('layout')
+@click.argument('communities_path')
+@click.option('--colors', default=None)
+@click.option('--level', default=0, show_default=True)
+@click.argument('outfile_path')
+def main(layout, communities_path, colors, level, outfile_path):
+    inextension = '.json'
+    if inextension in communities_path:
+        communities = communities_path
+        outfile = outfile_path
+        plot_communities_layout(layout, communities, colors, level, outfile)
+    else:
+        files = [f for f in os.listdir(communities_path) if inextension in f]
+        for f in tqdm(files):
+            communities = communities_path + f
+            outfile = outfile_path + f.split(inextension)[0]
+            plot_communities_layout(
+                layout, communities, colors, level, outfile
+            )
+
+
+if __name__ == '__main__':
+    main()
