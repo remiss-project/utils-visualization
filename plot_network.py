6d59da8b1a855b9b57a275e1afba1d2b94b53774
Joan Massachs <joan.massachs@eurecat.org>
2023-04-20 11:15:30 +0200

    plot network que es pugui cridar de fora

--- plot_network.py
+++ plot_network.py
@@ -11,15 +11,7 @@ def get_default_colors():
     return os.path.join(directory, 'default_community_colors.json')
 
 
-@click.command()
-@click.argument('layout', type=click.File('r'))
-@click.argument('communities', type=click.File('r'))
-@click.option('--colors', type=click.File('r'), default=get_default_colors())
-@click.argument('outfile', type=click.Path())
-def main(layout, communities, colors, outfile):
-    layout = json.load(layout)
-    comm = json.load(communities)
-    colors = json.load(colors)
+def plot_network(layout, comm, colors, outfile):
     nodes = sorted(list(layout))
     x = [layout[n][0] for n in nodes if n in comm]
     y = [layout[n][1] for n in nodes if n in comm]
@@ -36,6 +28,18 @@ def main(layout, communities, colors, outfile):
     plt.tight_layout()
     fig.savefig(outfile + '.pdf')
     fig.savefig(outfile + '.png')
+
+
+@click.command()
+@click.argument('layout', type=click.File('r'))
+@click.argument('communities', type=click.File('r'))
+@click.option('--colors', type=click.File('r'), default=get_default_colors())
+@click.argument('outfile', type=click.Path())
+def main(layout, communities, colors, outfile):
+    layout = json.load(layout)
+    comm = json.load(communities)
+    colors = json.load(colors)
+    plot_network(layout, comm, colors, outfile)
 
 
 if __name__ == '__main__':
