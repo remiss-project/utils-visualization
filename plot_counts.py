5da5d6049769bb5e891f6ecb213ffbe4cb7df596
Joan Massachs <joan.massachs@eurecat.org>
2023-06-07 16:12:59 +0200

    plot counts: opció per ylabel

--- plot_counts.py
+++ plot_counts.py
@@ -10,21 +10,22 @@ from matplotlib import dates
 @click.argument('outfile', type=click.Path())
 @click.option('--xlocator', default=7, show_default=True)
 @click.option('--group', is_flag=True)
-def main(infile, outfile, xlocator, group):
+@click.option('--ylabel', default='tweets', show_default=True)
+def main(infile, outfile, xlocator, group, ylabel):
     counts = pd.read_csv(infile)
     granularity = counts.columns[-1].split('_')[0]
     y = granularity + '_count'
     counts['start'] = counts['start'].apply(lambda x: x[:10])
     counts = counts.sort_values(['start', y], ascending=[True, False])
-    if 'query' in counts.columns and not group:
-        ax = sns.lineplot(data=counts, x='start', y=y, hue='query')
+    if counts.columns[0] != 'start' and not group:
+        ax = sns.lineplot(data=counts, x='start', y=y, hue=counts.columns[0])
     else:
         counts = counts[['start', y]].groupby('start').sum()
         ax = sns.lineplot(data=counts, x='start', y=y)
     ax.xaxis.set_major_locator(dates.DayLocator(interval=xlocator))
     plt.xticks(rotation=90)
     plt.xlabel(granularity)
-    plt.ylabel('tweets')
+    plt.ylabel(ylabel)
     plt.tight_layout()
     ax.get_figure().savefig(outfile)
 
